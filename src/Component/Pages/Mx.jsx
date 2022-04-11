d70023918eee68abf0be753ebb977ab0afd1b34c
Alexander <zhukoff.alexander@gmail.com>
2022-04-11 20:35:27 +0300

    refactor: Use FireBase database and Axios library

--- src/Component/Pages/Mx.jsx
+++ src/Component/Pages/Mx.jsx
@@ -2,22 +2,25 @@ import { Component } from 'react';
 import { Header } from '../Header';
 import MotoBar from '../MotoBar/MotoBar';
 import { MotoList } from '../MotoList';
+import axios from 'axios';
 
 const Mx = (props) => {
-  const moto = props.moto;
-  return (
-    <div>
-      <Header
-        category={props.category}
-        description={props.description}
-        length={moto.length}
-        background={props.background}
-      />
-      <MotoBar />
+  {
+    const moto = props.moto;
+    return (
+      <div>
+        <Header
+          category={props.category}
+          description={props.description}
+          length={moto.length}
+          background={props.background}
+        />
+        <MotoBar />
 
-      <MotoList moto={moto} category={props.category} />
-    </div>
-  );
+        <MotoList moto={moto} category={props.category} />
+      </div>
+    );
+  }
 };
 
 export { Mx };
